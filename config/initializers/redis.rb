849f3dd47617dc11af32f45568a51191918fbd1e
FanyingYe <yefanying@yefanyingdeMacBook-Pro.local>
2016-04-05 16:10:26 -0400

    redis try_1

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
-$redis = Redis.new(:host => 'localhost', :port => 6379)
+$redis = Redis.new(url: ENV["REDIS_URL"])
 Incident.all.each do |source|
 	$redis.zadd('incidents', source.created_at.to_i, source.to_json)
 end 
