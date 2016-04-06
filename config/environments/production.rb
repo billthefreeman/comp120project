e17caba6a2e110fc2d4eee88f5b3f4b5c5537ffb
FanyingYe <yefanying@yefanyingdeMBP.fios-router.home>
2016-04-06 10:24:20 -0400

    add image path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,7 @@ Rails.application.configure do
   # config/environments/production.rb
   config.paperclip_defaults = {
     :storage => :s3,
+    :path => "/image/:id/:filename",
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
