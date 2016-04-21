ae1e137bff484dedf87395c3d143b377eee1eb1d
FanyingYe <yefanying@yefanyingdeMBP.fios-router.home>
2016-04-20 23:34:38 -0400

    final10

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,14 @@
 $redis = Redis.new(url: ENV["REDIS_URL" || "redis://localhost:6379/"])
+
+$redis.del("incidents")
+$redis.del("people")
+$redis.del("phone")
+$redis.del("email")
+$redis.del("people_group")
+$redis.del("group_department")
+$redis.del("cates")
+$redis.del("statuses")
+
 Incident.all.each do |source|
 	$redis.zadd('incidents', source.created_at.to_i, source.to_json)
 end 
